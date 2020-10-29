d589b26da2653ec4b036b8864e795854231d2291
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-29 10:54:50 -0400

    Wrap functions that prompts the user in an init() function and call that function.

--- app.js
+++ app.js
@@ -12,141 +12,151 @@ const render = require("./lib/htmlRenderer");
 
 const teamMembers = [];
 
+// Initialize the application to gather information about the team members
+function init(){
+    // Add prompts to get information about the manager
+    function addManager(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter your manager's name:",
+                name: "managerName",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your manager's ID:",
+                name: "managerId",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your manager's email address:",
+                name: "managerEmail",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your manager's office number:",
+                name: "managerOffice",
+                validate: validateInput
+            }
+        ]).then(answers => {
+            // Get the data from the user's input
+            const { managerName, managerId, managerEmail, managerOffice } = answers;
 
-// Add prompts to get information about the manager
-function addManager(){
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter your manager's name:",
-            name: "managerName",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your manager's ID:",
-            name: "managerId",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your manager's email address:",
-            name: "managerEmail",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your manager's office number:",
-            name: "managerOffice",
-            validate: validateInput
-        }
-    ]).then(answers => {
-        // Get the data from the user's input
-        const { managerName, managerId, managerEmail, managerOffice } = answers;
+            // Create a new Manager instance and add it to the teamMembers array
+            const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
+            teamMembers.push(manager);
 
-        // Create a new Manager instance and add it to the teamMembers array
-        const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
-        teamMembers.push(manager);
+            // Call the create team to add any engineers or interns to the team
+            createTeam();
+        });
+    }
 
-        createTeam();
-    });
+    // Add a prompt to see if the user needs to add a new intern/engineer object or if they are done
+    function createTeam(){
+        inquirer.prompt([
+            {
+                type:"list",
+                message: "Add a new team member:",
+                name: "newMember",
+                choices: ["Intern", "Engineer", "I do not want to add a new team member"]
+            }
+        ]).then(answer => {
+            if(answer.newMember === "Intern"){
+                addIntern();
+            }
+            else if(answer.newMember === "Engineer"){
+                addEngineer();
+            }
+            else {
+                renderTeam();
+            }
+        })
+    }
+
+    // Add prompts to get information about the intern
+    function addIntern(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter your intern's name:",
+                name: "internName",
+                validate: validateInput
+            },
+            {
+                type: "number",
+                message: "Enter your intern's ID:",
+                name: "internId",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your intern's email address:",
+                name: "internEmail",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your intern's school name:",
+                name: "internSchool",
+                validate: validateInput
+            }
+        ]).then(answers => {
+            const { internName, internId, internEmail, internSchool } = answers;
+
+            const intern = new Intern(internName, internId, internEmail, internSchool);
+            teamMembers.push(intern);
+
+            createTeam();
+        });
+    }
+
+    // Add prompts to get information about the engineer
+    function addEngineer(){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter your engineer's name:",
+                name: "engineerName",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your engineer's ID:",
+                name: "engineerId",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your engineer's email address:",
+                name: "engineerEmail",
+                validate: validateInput
+            },
+            {
+                type: "input",
+                message: "Enter your engineer's GitHub username:",
+                name: "engineerGithub",
+                validate: validateInput
+            }
+        ]).then(answers => {
+            const { engineerName, engineerId, engineerEmail, engineerGithub } = answers;
+
+            const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
+            teamMembers.push(engineer);
+
+            createTeam();
+        });
+    }
+
+    addManager();
 }
 
-// Add a prompt to see if the user needs to add a new intern/engineer object or if they are done
-function createTeam(){
-    inquirer.prompt([
-        {
-            type:"list",
-            message: "Add a new team member:",
-            name: "newMember",
-            choices: ["Intern", "Engineer", "I do not want to add a new team member"]
-        }
-    ]).then(answer => {
-        if(answer.newMember === "Intern"){
-            addIntern();
-        }
-        else if(answer.newMember === "Engineer"){
-            addEngineer();
-        }
-        else {
-            renderTeam();
-        }
-    })
-}
 
-// Add prompts to get information about the intern
-function addIntern(){
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter your intern's name:",
-            name: "internName",
-            validate: validateInput
-        },
-        {
-            type: "number",
-            message: "Enter your intern's ID:",
-            name: "internId",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your intern's email address:",
-            name: "internEmail",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your intern's school name:",
-            name: "internSchool",
-            validate: validateInput
-        }
-    ]).then(answers => {
-        const { internName, internId, internEmail, internSchool } = answers;
+// Initialize the application, start with getting info on the manager
+init();
 
-        const intern = new Intern(internName, internId, internEmail, internSchool);
-        teamMembers.push(intern);
-
-        createTeam();
-    });
-}
-
-// Add prompts to get information about the engineer
-function addEngineer(){
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter your engineer's name:",
-            name: "engineerName",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your engineer's ID:",
-            name: "engineerId",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your engineer's email address:",
-            name: "engineerEmail",
-            validate: validateInput
-        },
-        {
-            type: "input",
-            message: "Enter your engineer's GitHub username:",
-            name: "engineerGithub",
-            validate: validateInput
-        }
-    ]).then(answers => {
-        const { engineerName, engineerId, engineerEmail, engineerGithub } = answers;
-
-        const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
-        teamMembers.push(engineer);
-
-        createTeam();
-    });
-}
 
 // Write to a file (new or existing) called team.html in the output directory with the user's inputs rendered into html
 function renderTeam() {
@@ -156,8 +166,6 @@ function renderTeam() {
     });
 }
 
-// Initialize the application, start with getting info on the manager
-addManager();
 
 // Validation
 function validateInput(input){
